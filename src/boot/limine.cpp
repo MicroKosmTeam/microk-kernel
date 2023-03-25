bd3b870bca1337805152d57d694b5b4c23282365
Filippo Mutta <filippo.mutta@gmail.com>
2023-03-25 14:22:53 +0100

    Minor changes.

--- src/boot/limine.cpp
+++ src/boot/limine.cpp
@@ -4,12 +4,32 @@
 #include <boot/boot.hpp>
 #include <sys/printk.hpp>
 
+/* Function called by Limine */
 extern "C" void LimineEntry(void);
 
+/*
+   Setting the correct entry point for Limine
+*/
 static volatile limine_entry_point_request entryPointRequest {
 	.id = LIMINE_ENTRY_POINT_REQUEST,
 	.revision = 0,
 	.entry = &LimineEntry
+};
+
+/*
+   System time request for Limine
+*/
+static volatile limine_boot_time_request timeRequest {
+	.id = LIMINE_BOOT_TIME_REQUEST,
+	.revision = 0
+};
+
+/*
+   Bootloader info request for Limine
+*/
+static volatile limine_bootloader_info_request bootloaderRequest {
+	.id = LIMINE_BOOTLOADER_INFO_REQUEST,
+	.revision = 0
 };
 
 /*
@@ -23,14 +43,23 @@ static volatile limine_stack_size_request stackRequest {
 };
 
 extern "C" void LimineEntry() {
+	/* Startup basic kernel runtime services */
 	EarlyKernelInit();
 
-	PRINTK::PrintK("Welcome from MicroK Limine.\r\n");
-
-	/* Checking if the stack request has been answered by Limine */
+	/* Checking requests have been answered by Limine */
 	/* If not, give up and shut down */
-	if (stackRequest.response == NULL)
-		PANIC("Stack size request not answered by Limine");
+	if(timeRequest.response == NULL ||
+	   stackRequest.response == NULL ||
+	   bootloaderRequest.response == NULL)
+		PANIC("Requests not answered by Limine");
 
+
+	PRINTK::PrintK("Welcome from MicroK.\r\n"
+		       "The kernel is booted by %s %s at %d\r\n",
+		       bootloaderRequest.response->name,
+		       bootloaderRequest.response->version,
+		       timeRequest.response->boot_time);
+
+	/* Launch the kernel proper */
 	KernelStart();
 }
