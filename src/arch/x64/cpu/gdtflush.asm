dd7a78a31fd414371e364ac9d960b03432ffac1b
Filippo Mutta <filippo.mutta@gmail.com>
2023-08-15 18:36:16 +0200

    Fixed gdt code.

--- src/arch/x64/cpu/gdtflush.asm
+++ src/arch/x64/cpu/gdtflush.asm
@@ -4,17 +4,22 @@
 global FlushGDT
 FlushGDT:
 	lgdt [rdi]
-	mov ax, 0x30
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	pop rdi
-	mov rax, 0x28
-	push rax
-	push rdi
-	retq
+
+reloadSegments:
+	; Reload CS register:
+	push 0x28                 ; Push code segment to stack, 0x08 is a stand-in for your code segment
+	lea rax, [rel .reloadCS]  ; Load address of .reload_CS into RAX
+	push rax                  ; Push this value to the stack
+	o64 retf                  ; Perform a far return
+.reloadCS:
+	; Reload data segment registers
+	mov   ax, 0x30
+	mov   ds, ax
+	mov   es, ax
+	mov   fs, ax
+	mov   gs, ax
+	mov   ss, ax
+	ret
 
 ; Function to load the TSS
 global FlushTSS
