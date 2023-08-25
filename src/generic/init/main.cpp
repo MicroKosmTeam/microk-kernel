47526b44975c57ca6daed8e9574925fc6955b2bc
Filippo Mutta <filippo.mutta@gmail.com>
2023-08-25 12:45:14 +0200

    Added comments to source and header files in init/

--- src/generic/init/main.cpp
+++ src/generic/init/main.cpp
@@ -1,27 +1,27 @@
-/*
-   File: init/main.cpp
-   __  __  _                _  __        ___   ___
-  |  \/  |(_) __  _ _  ___ | |/ /       / _ \ / __|
-  | |\/| || |/ _|| '_|/ _ \|   <       | (_) |\__ \
-  |_|  |_||_|\__||_|  \___/|_|\_\       \___/ |___/
+/*  __  __  _                _  __        ___   ___
+ * |  \/  |(_) __  _ _  ___ | |/ /       / _ \ / __|
+ * | |\/| || |/ _|| '_|/ _ \|   <       | (_) |\__ \
+ * |_|  |_||_|\__||_|  \___/|_|\_\       \___/ |___/
+ *
+ * MicroKernel, a simple futiristic Unix-shattering kernel
+ * Copyright (C) 2022-2023 Mutta Filippo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
 
-  MicroKernel, a simple futiristic Unix-shattering kernel
-  Copyright (C) 2022-2023 Mutta Filippo
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General does return to userspacePublic License for more details.
-
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General does return to userspacePublic License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * File: generic/init/main.cpp
+ */
 
 #include <cdefs.h>
 #include <stdint.h>
@@ -46,32 +46,26 @@
 #include <arch/x64/main.hpp>
 #elif defined(ARCH_aarch64)
 #include <arch/aarch64/main.hpp>
-extern "C" void __aarch64_cas1_sync() {}
-extern "C" void CPUPause() {}
-extern "C" void EnterUserspace() {}
 #endif
 
-/*
-   Function that is started by the scheduler once kernel startup is complete.
-*/
-void RestInit();
-__attribute__((__aligned__((16)))) char RestStack[4096];
 
 int EarlyKernelInit() {
 	/* Allocating memory for the info struct */
 	InitInfo();
+	KInfo *info = GetInfo();
 
+	/* Initializing early architecture-specific devices */
 #if defined(ARCH_x64)
 	x86_64::EarlyInit();
 #elif defined(ARCH_aarch64)
 	AArch64::EarlyInit();
 #endif
-
 	/* Loading early serial printk */
 	PRINTK::EarlyInit();
 
 	return 0;
 }
+
 
 void PrintBanner() {
 	/* Getting some useful system info */
@@ -100,22 +94,18 @@ void PrintBanner() {
 			BOOTMEM::GetTotal() - BOOTMEM::GetFree());
 }
 
-/*
-   Main kernel function.
-*/
+
 __attribute__((noreturn)) void KernelStart() {
 	KInfo *info = GetInfo();
 
 	PRINTK::PrintK("MicroKosm is booted. Cmdline: %s\r\n", info->KernelArgs);
-
 	ParseArgs();
-
-	MEM::DisplayRam();
 
 	/* Enabling the page frame allocator */
 	PMM::InitPageFrameAllocator();
 
 	/* Starting the memory subsystem */
+	MEM::DisplayRam();
 	MEM::Init();
 
 	/* Initializing the heap */
@@ -124,11 +114,10 @@ __attribute__((noreturn)) void KernelStart() {
 	/* With the heap initialized, disable new bootmem allocations */
 	BOOTMEM::DeactivateBootmem();
 
+	/* Starting architecture-specific instructions */
 #if defined(ARCH_x64)
-	/* Starting x86_64 specific instructions */
 	x86_64::Init();
 #elif defined(ARCH_aarch64)
-	/* Starting AArch64 specific instructions */
 	AArch64::Init();
 #endif
 
@@ -150,16 +139,18 @@ __attribute__((noreturn)) void KernelStart() {
 	size_t moduleSize;
 	void *addr;
 
-	/* Get the core module's file memory location */
+	/* Get the memory location of the user module */
 	addr = FILE::Open(info->UserModuleName, &moduleSize);
 	
-	/* Launch the core user core module */
+	/* Check if it is present */
 	if (addr != NULL) {
 		PRINTK::PrintK("Loading user module from 0x%x\r\n", addr);
 
+		/* Create the process and set it to a ready state */
 		size_t pid = LoadExecutableFile(addr, moduleSize);
 		info->kernelScheduler->SetProcessState(pid, PROC::P_READY);
 
+		/* Recalculate the scheduler and wait for the context switch */
 		PRINTK::PrintK("Switching to user module.\r\n");
 		info->kernelScheduler->RecalculateScheduler();
 	} else PANIC("Could not find User Module");
@@ -167,14 +158,4 @@ __attribute__((noreturn)) void KernelStart() {
 
 	/* We are done */
 	while (true) CPUPause();
-}
-
-__attribute__((noreturn)) void RestInit() {
-	KInfo *info = GetInfo();
-
-	PRINTK::PrintK("Working...\r\n");
-
-	info->kernelScheduler->RecalculateScheduler();
-
-	while(true);
 }
