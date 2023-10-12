cbca5aa91cbff063ebe9cb490f5bb55aa23e95dc
Filippo Mutta <filippo.mutta@gmail.com>
2023-10-12 18:02:51 +0200

    Generic timer framework.

---
+++ src/include/sys/time.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <cdefs.h>
+#include <stdint.h>
+#include <stddef.h>
+
+typedef intmax_t ticks_t;
+
+namespace TIME {
+	struct Timer {
+		int (*Initialize)(Timer *timer);
+
+		ticks_t (*GetTicks)(Timer *timer);
+		ticks_t (*GetDivider)(Timer *timer);
+
+		int (*WaitSync)(Timer *timer, ticks_t ticks);
+	};
+
+	/* Tells the timer to initialze itself */
+	int InitializeTimer(Timer *timer);
+
+	/* Returns the total amount of elapsed ticks */
+	ticks_t GetTimerTicks(Timer *timer);
+	
+	/* Returns the number of ticks per second */
+	ticks_t GetTimerDivider(Timer *timer);
+
+	/* Waits for the time to expire */
+	int WaitSync(Timer *timer, ticks_t ticks);
+}
