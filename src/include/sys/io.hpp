bc8d22ee82ae20dda64ad99215f83238ecbe89a1
Filippo Mutta <filippo.mutta@gmail.com>
2023-10-16 15:33:41 +0200

    Added OutB and InB functions for architectures that have dedicated IO ports.

--- src/include/sys/io.hpp
+++ src/include/sys/io.hpp
@@ -5,6 +5,10 @@
 #include <sys/user.hpp>
 #include <sys/printk.hpp>
 
+#if defined(ARCH_x64)
+#include <arch/x64/io/io.hpp>
+#endif
+
 struct IORequest {
 	uint8_t MemoryAccessType;
 	uintptr_t Address;
@@ -12,3 +16,19 @@ struct IORequest {
 }__attribute__((packed));
 
 int ExecuteIORequest(IORequest requests[], size_t requestCount);
+
+#if !defined(ARCH_AArch64)
+inline void OutB(uintptr_t port, uint8_t byte) {
+#if defined(ARCH_x64)
+	x86_64::OutB(port, byte);
+#endif
+}
+
+inline uint8_t InB(uintptr_t port) {
+#if defined(ARCH_x64)
+	return x86_64::InB(port);
+#endif
+}
+
+#endif
+
