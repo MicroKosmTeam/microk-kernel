bdf8520b02f847f281102f962610e7e67ef1e9c5
Filippo Mutta <filippo.mutta@gmail.com>
2023-10-25 18:32:02 +0200

    Changed names in the MMap region structure, added all memory initialization steps in MEM::Init, changes bootmem allocation as necessarily explicit (not allocatable by calling normal Malloc or new)

--- src/generic/mm/memory.cpp
+++ src/generic/mm/memory.cpp
@@ -7,20 +7,8 @@
 #include <mm/heap.hpp>
 #include <init/kinfo.hpp>
 
-static char *memTypeStrings[] = {
-	"Usable",
-	"Reserved",
-	"ACPI Reclaimable",
-	"ACPI NVS",
-	"Bad",
-	"Bootloader reclaimable",
-	"Kernel and modules",
-	"Framebuffer"
-};
-
 void *Malloc(usize size) {
 	if(HEAP::IsHeapActive()) return HEAP::Malloc(size);
-	if(BOOTMEM::BootmemIsActive()) return BOOTMEM::Malloc(size);
 	else return NULL;
 }
 
@@ -30,82 +18,81 @@ void Free(void *p) {
 
 void *operator new(usize size) {
 	if(HEAP::IsHeapActive()) return HEAP::Malloc(size);
-	if(BOOTMEM::BootmemIsActive()) return BOOTMEM::Malloc(size);
 	else return NULL;
 }
 
 void *operator new[](usize size) {
 	if(HEAP::IsHeapActive()) return HEAP::Malloc(size);
-	if(BOOTMEM::BootmemIsActive()) return BOOTMEM::Malloc(size);
 	else return NULL;
 }
 
 void operator delete(void* p) {
-	// Now, here comes the problem in deciding who allocated this block
-	// We should assume that someone that allocs on BOOTMEM
-	// will not call free
-
-	HEAP::Free(p);
+	if(HEAP::IsHeapActive()) HEAP::Free(p);
 }
 
 void operator delete(void* p, usize size) {
-	// Now, here comes the problem in deciding who allocated this block
-	// We should assume that someone that allocs on BOOTMEM
-	// will not call free
-
 	(void)size;
-	HEAP::Free(p);
+	if(HEAP::IsHeapActive()) HEAP::Free(p);
 }
 
 void operator delete[](void* p) {
-	// Now, here comes the problem in deciding who allocated this block
-	// We should assume that someone that allocs on BOOTMEM
-	// will not call free
-
-	HEAP::Free(p);
+	if(HEAP::IsHeapActive()) HEAP::Free(p);
 }
 
 void operator delete[](void* p, usize size) {
-	// Now, here comes the problem in deciding who allocated this block
-	// We should assume that someone that allocs on BOOTMEM
-	// will not call free
-
 	(void)size;
-	HEAP::Free(p);
+	if(HEAP::IsHeapActive()) HEAP::Free(p);
 }
 
 namespace MEM {
+void InvokeOOM() {
+	PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, "OOM invoked, system is out of memory.\r\n");
 
-void DisplayRam() {
+	PANIC("Out of memory");
+}
+
+void Init() {
+	char *memTypeStrings[] = {
+		"Usable",
+		"Reserved",
+		"ACPI Reclaimable",
+		"ACPI NVS",
+		"Bad",
+		"Bootloader reclaimable",
+		"Kernel and modules",
+		"Framebuffer"
+	};
+
+
 	KInfo *info = GetInfo();
 
 	PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, "Provided physical RAM map:\r\n");
 
 	for (usize i = 0; i < info->MemoryMapEntryCount; i++) {
 		PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, " [0x%x - 0x%x] -> %s\r\n",
-				info->MemoryMap[i].base,
-				info->MemoryMap[i].base + info->MemoryMap[i].length,
-				memTypeStrings[info->MemoryMap[i].type]);
+				info->MemoryMap[i].AddressBase,
+				info->MemoryMap[i].AddressBase + info->MemoryMap[i].Length,
+				memTypeStrings[info->MemoryMap[i].Type]);
 	}
 
 	PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, "Contiguous regions:\r\n");
 	bool isAllUnusable = true;
-	uptr base = info->MemoryMap[0].base;
-	usize length = info->MemoryMap[0].length;
+	uptr base = info->MemoryMap[0].AddressBase;
+	usize length = info->MemoryMap[0].Length;
 
 	for (usize region = 1; region < info->MemoryMapEntryCount; region++) {
-		if (info->MemoryMap[region - 1].base + info->MemoryMap[region - 1].length == info->MemoryMap[region].base) {
-			length += info->MemoryMap[region].length;
-			if(isAllUnusable) isAllUnusable = info->MemoryMap[region].type == MEMMAP_USABLE ? false : true;
+		if (info->MemoryMap[region - 1].AddressBase + info->MemoryMap[region - 1].Length == info->MemoryMap[region].AddressBase) {
+			length += info->MemoryMap[region].Length;
+			if(isAllUnusable) isAllUnusable = info->MemoryMap[region].Type == MEMMAP_USABLE ? false : true;
 		} else {
 			PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, " %s area: [0x%x - 0x%x]\r\n",
 				isAllUnusable ? "Unusable" : "Usable",
 				base,
 				base + length);
 
-			base = info->MemoryMap[region].base;
-			length = info->MemoryMap[region].length;
-			isAllUnusable = info->MemoryMap[region].type == MEMMAP_USABLE ? false : true;
+			base = info->MemoryMap[region].AddressBase;
+			length = info->MemoryMap[region].Length;
+			isAllUnusable = info->MemoryMap[region].Type == MEMMAP_USABLE ? false : true;
 		}
 	}
 
@@ -114,16 +101,15 @@ void DisplayRam() {
 				base,
 				base + length);
 
-}
+	/* Enabling the page frame allocator */
+	PMM::InitPageFrameAllocator();
 
-
-void InvokeOOM() {
-	PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, "OOM invoked, system is out of memory.\r\n");
-
-	PANIC("Out of memory");
-}
-
-void Init() {
 	VMM::InitVMM();
+
+	/* Initializing the heap */
+	HEAP::InitializeHeap((void*)CONFIG_HEAP_BASE, CONFIG_HEAP_SIZE / PAGE_SIZE);
+
+	/* With the heap initialized, disable new bootmem allocations */
+	BOOTMEM::DeactivateBootmem();
 }
 }
--- src/include/mm/memory.hpp
+++ src/include/mm/memory.hpp
@@ -1,9 +1,17 @@
 #pragma once
 #include <cstdint.hpp>
 
+#define MEMMAP_USABLE                 0
+#define MEMMAP_RESERVED               1
+#define MEMMAP_ACPI_RECLAIMABLE       2
+#define MEMMAP_ACPI_NVS               3
+#define MEMMAP_BAD_MEMORY             4
+#define MEMMAP_BOOTLOADER_RECLAIMABLE 5
+#define MEMMAP_KERNEL_AND_MODULES     6
+#define MEMMAP_FRAMEBUFFER            7
 
-void *Memcpy(void *dest, void *src, usize n);
 void *Memset(void *start, u8 value, u64 num);
+void *Memcpy(void *dest, void *src, usize n);
 int Memcmp(const void* buf1, const void* buf2, usize count);
 void *Memmove(void *dest, const void *src, usize n);
 
@@ -18,22 +26,12 @@ void operator delete[](void* p);
 void operator delete[](void* p, usize size);
 
 namespace MEM {
-	#define MEMMAP_USABLE                 0
-	#define MEMMAP_RESERVED               1
-	#define MEMMAP_ACPI_RECLAIMABLE       2
-	#define MEMMAP_ACPI_NVS               3
-	#define MEMMAP_BAD_MEMORY             4
-	#define MEMMAP_BOOTLOADER_RECLAIMABLE 5
-	#define MEMMAP_KERNEL_AND_MODULES     6
-	#define MEMMAP_FRAMEBUFFER            7
-
 	struct MMapEntry {
-		uptr base;
-		uptr length;
-		uptr type;
+		uptr AddressBase;
+		uptr Length;
+		u8 Type;
 	};
 
 	void Init();
 	void InvokeOOM();
-	void DisplayRam();
 }
