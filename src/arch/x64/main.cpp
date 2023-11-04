79b434611b532de4b5809d5d0c794a7196031aad
Filippo Mutta <filippo.mutta@gmail.com>
2023-11-04 11:30:14 +0100

    Moved initialization functionality to the CPU module itself.

--- src/arch/x64/main.cpp
+++ src/arch/x64/main.cpp
@@ -15,18 +15,9 @@ namespace x86_64 {
 void Init() {
 	KInfo *info = GetInfo();
 
-	info->KernelStack = VMM::PhysicalToVirtual((uptr)PMM::RequestPages(KERNEL_STACK_SIZE / PAGE_SIZE) + KERNEL_STACK_SIZE);
-	PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, "CPU interrupt stack: 0x%x\r\n", info->KernelStack);
-
-	/* Initialization of the TSS */
-	TSSInit(info->KernelStack);
-
 	/* x86 CPU initialization */
-	CPUInit();
-
-	/* We get the CPU vendor */
-	PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, "CPU Vendor: %s\r\n", GetCPUVendor());
-
+	UpdatePerCPUStack(info->BootCore, KERNEL_STACK_SIZE);
+	CurrentCPUInit(info->BootCore);
 	InitDevices();
 }
 }
