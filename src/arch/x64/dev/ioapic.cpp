d6cd4be2ee55317ac322c85276780b83ca7926aa
Filippo Mutta <filippo.mutta@gmail.com>
2023-11-04 11:29:39 +0100

    Added basic IOAPIC initialization functions.

--- src/arch/x64/dev/ioapic.cpp
+++ src/arch/x64/dev/ioapic.cpp
@@ -1,15 +1,32 @@
 #include <arch/x64/dev/ioapic.hpp>
+#include <stdarg.h>
+#include <init/kinfo.hpp>
+#include <sys/printk.hpp>
 
-namespace x86_64 {
+namespace x86_64::IOAPIC {
 DEV::Device *CreateIOAPICDevice() {
 	IOAPIC *ioapic = new IOAPIC;
+
+	ioapic->Initialize = InitializeDevice;
+	ioapic->Deinitialize = DeinitializeDevice;
+	ioapic->Ioctl = Ioctl;
 	
 	return (DEV::Device*)ioapic;
 }
 
 int InitializeDevice(DEV::Device *device, va_list ap) {
-	(void)device;
-	(void)ap;
+	KInfo *info = GetInfo();
+	IOAPIC *ioapic = (IOAPIC*)device;
+	
+	ioapic->ID = va_arg(ap, u32);
+
+	ioapic->Base = va_arg(ap, uptr);
+	ioapic->MappedAddress = VMM::PhysicalToVirtual(ioapic->Base);
+	VMM::MapPage(info->KernelVirtualSpace, ioapic->Base, ioapic->MappedAddress, VMM_FLAGS_KERNEL_DEVICE);
+
+	ioapic->GlobalSystemInterruptBase = va_arg(ap, u32);
+
+	PRINTK::PrintK(PRINTK::DEBUG, MODULE_NAME, "IOAPIC 0x%x at 0x%x\r\n", ioapic->ID, ioapic->Base);
 
 	return 0;
 }
