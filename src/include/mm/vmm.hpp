35ea18b43a4ef0f70a4df406c04362243f9abbc9
Filippo Mutta <filippo.mutta@gmail.com>
2023-11-15 15:28:57 +0100

    Updated page protection constants.

--- src/include/mm/vmm.hpp
+++ src/include/mm/vmm.hpp
@@ -46,9 +46,8 @@ namespace VMM {
 	usize ConvertUserFlags(usize flags) {
 		usize ret = VMM_FLAGS_USER;
 
-		if (!(flags & PAGE_PROTECTION_NONE) &&
-		    flags & PAGE_PROTECTION_READ) {
-			ret |= VMM_FLAGS_ACCESSIBLE;
+		if (flags & PAGE_PROTECTION_NONE) {
+			return ret;
 		}
 		
 		if (flags & PAGE_PROTECTION_WRITE) {
@@ -57,10 +56,6 @@ namespace VMM {
 
 		if (!(flags & PAGE_PROTECTION_EXEC)) {
 			ret |= VMM_FLAGS_NOEXEC;
-		}
-
-		if (flags & PAGE_PROTECTION_NOCACHE) {
-			ret |= VMM_FLAGS_NOCACHE;
 		}
 
 		return ret;
