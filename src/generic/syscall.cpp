b67aebd68db193b9913c172c66f44a18100f37e7
Filippo Mutta <filippo.mutta@gmail.com>
2024-02-10 10:42:16 +0100

    Syscall testing.

--- src/generic/syscall.cpp
+++ src/generic/syscall.cpp
@@ -10,15 +10,20 @@ extern "C" void SyscallMain(usize syscallNumber, usize firstArgument, usize seco
 	(void)fithArgument;
 	(void)sixthArgument;
 
+	PRINTK::PrintK(PRINTK_DEBUG "Hello, syscall!\r\n");
+
 	switch (syscallNumber) {
 		case SYSCALL_VECTOR_DEBUG:
-			PRINTK::PrintK(PRINTK_DEBUG "Hello, syscall!\r\n");
+			PRINTK::PrintK(PRINTK_DEBUG " -> Debug\r\n");
 			break;
 		case SYSCALL_VECTOR_YEILD:
+			PRINTK::PrintK(PRINTK_DEBUG " -> Yeild\r\n");
 			break;
 		case SYSCALL_VECTOR_CALL:
+			PRINTK::PrintK(PRINTK_DEBUG " -> Call\r\n");
 			break;
 		case SYSCALL_VECTOR_REPLYRECV:
+			PRINTK::PrintK(PRINTK_DEBUG " -> ReplyRecv\r\n");
 			break;
 		default:
 			break;
